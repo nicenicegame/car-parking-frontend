eba8a8361cc3e4aedda0a1e2ff58a1c74e1bb6b9
Nice <nicenicegame@gmail.com>
2021-02-13 11:24:33 +0700

    Added stop timer

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+let data = [
+  {
+    id: 1,
+    status: 0,
+    time: null,
+  },
+  {
+    id: 2,
+    status: 0,
+    time: null,
+  },
+  {
+    id: 3,
+    status: 1,
+    time: null,
+  },
+  {
+    id: 4,
+    status: 1,
+    time: null,
+  },
+]
+
 const parkingLots = document.querySelectorAll('.lot')
 
 function countTime(lot) {
@@ -5,7 +28,7 @@ function countTime(lot) {
   timer.style.display = 'block'
   const timeText = timer.children[0]
   let second = 0
-  setInterval(() => {
+  return setInterval(() => {
     timeText.innerText = getTime(second)
     second++
   }, 1000)
@@ -17,5 +40,53 @@ function getTime(second) {
   )
 }
 
-countTime(parkingLots[2])
-countTime(parkingLots[3])
+const timer = [null, null, null, null]
+const prevState = [0, 0, 0, 0]
+
+function start() {
+  data.forEach((lotData) => {
+    if (lotData.status === 1 && prevState[lotData.id - 1] === 0) {
+      prevState[lotData.id - 1] = 1
+      const activeLot = parkingLots[lotData.id - 1]
+      activeLot.classList.add('active')
+      timer[lotData.id - 1] = countTime(activeLot)
+    } else if (lotData.status === 0 && prevState[lotData.id - 1] === 1) {
+      const activeLot = parkingLots[lotData.id - 1]
+      activeLot.classList.remove('active')
+      console.log('Stopped')
+      clearInterval(timer[lotData.id - 1])
+    }
+  })
+}
+
+setTimeout(() => {
+  data = [
+    {
+      id: 1,
+      status: 0,
+      time: null,
+    },
+    {
+      id: 2,
+      status: 0,
+      time: null,
+    },
+    {
+      id: 3,
+      status: 0,
+      time: null,
+    },
+    {
+      id: 4,
+      status: 1,
+      time: null,
+    },
+  ]
+  console.log(data[2])
+}, 5000)
+
+// countTime(parkingLots[2])
+// countTime(parkingLots[3])
+setInterval(() => {
+  start()
+}, 1000)
