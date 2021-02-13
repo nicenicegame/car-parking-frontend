65e7c2bbeb515889d1ccceb7bedac5ae4fc7033f
unknown <pun.hoo.hoo@gmail.com>
2021-02-13 12:02:04 +0700

    Try graph.js is demo

--- graph.js
+++ graph.js
@@ -1,28 +1,4 @@
-/*const parkingLots = document.querySelectorAll('.lot')
-
-function countTime(lot) {
-  const timer = lot.querySelector('.timer')
-  timer.style.display = 'block'
-  const timeText = timer.children[0]
-  let second = 0
-  setInterval(() => {
-    timeText.innerText = getTime(second)
-    second++
-  }, 1000)
-}
-
-function getTime(second) {
-  return (
-    Math.floor(second / 60) + ':' + ('0' + Math.floor(second % 60)).slice(-2)
-  )
-}
-
-countTime(parkingLots[2])
-countTime(parkingLots[3])
-*/
 //graph 
-
-
 var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
     type: 'line',
@@ -60,4 +36,3 @@ var myChart = new Chart(ctx, {
         }
     }
 });
-
