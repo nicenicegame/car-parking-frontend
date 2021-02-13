8bb6bfc187201eb1fcbadf9384da70c065fc77d4
Nice <nicenicegame@gmail.com>
2021-02-13 10:36:38 +0700

    Added timer

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const parkingLots = document.querySelectorAll('.lot')
 
 function countTime(lot) {
-  const timer = lot.querySelectorAll('.timer p')
+  const timer = lot.querySelector('.timer')
+  timer.style.display = 'block'
+  const timeText = timer.children[0]
   let second = 0
   setInterval(() => {
-    timer.innerText = second
+    timeText.innerText = getTime(second)
     second++
-    console.log(second)
   }, 1000)
 }
 
-console.log(parkingLots[0])
+function getTime(second) {
+  return (
+    Math.floor(second / 60) + ':' + ('0' + Math.floor(second % 60)).slice(-2)
+  )
+}
 
-// countTime(parkingLots[0])
+countTime(parkingLots[2])
+countTime(parkingLots[3])
