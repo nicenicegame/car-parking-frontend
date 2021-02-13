2e807884c492ca3468d070a3574d78e644dd90e2
unknown <pun.hoo.hoo@gmail.com>
2021-02-13 16:09:15 +0700

    try graph plot idea in graph.js

--- graph.js
+++ graph.js
@@ -1,11 +1,50 @@
 
-var dataset = []
+var assume1 = [1,1,1,1,1,0,0,0,0,0,1,1,1,1]
+var assume2 = [1,1,1,0,0,0,1,0,0,0,1,1,1,1]
+var assume3 = [1,1,1,1,1,0,0,0,1,1,1,1,1,1]
+var assume4 = [1,1,1,1,1,1,0,0,0,1,1,1,1,1]
+
+var init1= 0
+var init2= 0
+var init3= 0
+var init4= 0
+
+var dataset1 = []
+var dataset2 = []
+var dataset3 = []
+var dataset4 = []
+
 var labelset = []
 
-for(i = 0; i < 10; i++){
-    labelset.push("point"+ i)
-    dataset.push(i);    
+for(i in assume1){
+    if(assume1[i]==1){
+        init1 = init1+1;
+    }
+    if(assume2[i]==1){
+        init2 = init2+1;
+    }
+    if(assume3[i]==1){
+        init3 = init3+1;
+    }
+    if(assume4[i]==1){
+        init4 = init4+1;
+    }
+
+    dataset1.push(init1)
+    dataset2.push(init2)
+    dataset3.push(init3)
+    dataset4.push(init4)
+
+    labelset.push(i)
 }
+
+
+
+
+// for(i = 0; i < 10; i++){
+//     labelset.push("point"+ i)
+//     dataset.push(i);    
+// }
 //parkA
 var ctx = document.getElementById('myChart1');
 var myChart1 = new Chart(ctx, {
@@ -14,7 +53,7 @@ var myChart1 = new Chart(ctx, {
         labels: labelset,
         datasets: [{
             label: '# of ParkA',
-            data: dataset,
+            data: dataset1,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.6)',
                 
@@ -44,7 +83,7 @@ var myChart2 = new Chart(ctx2, {
         labels: labelset,
         datasets: [{
             label: '# of ParkB',
-            data: dataset,
+            data: dataset2,
             backgroundColor: [
                 'rgba(54, 162, 235, 0.6)'
             ],
@@ -73,7 +112,7 @@ var myChart3 = new Chart(ctx3, {
         labels: labelset,
         datasets: [{
             label: '# of ParkC',
-            data: dataset,
+            data: dataset3,
             backgroundColor: [
                 'rgba(255, 206, 86, 0.6)'
             ],
@@ -102,7 +141,7 @@ var myChart4 = new Chart(ctx4, {
         labels: labelset,
         datasets: [{
             label: '# of Park4',
-            data: dataset,
+            data: dataset4,
             backgroundColor: [
                 'rgba(75, 192, 192, 0.6)'
             ],
